13649f6721756f1743852431c03defaea6968e23
vi26 <44754162+vi26@users.noreply.github.com>
2018-11-05 18:24:35 +0300

    Изменение функций в методы соответствующих классов

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -8,10 +8,10 @@ public class MyFirstProgram {
     hello("Vitaly");
 
     Square s = new Square(5);
-    System.out.println("Площидь квадрата со стороной " + s.l + " = " + area(s));
+    System.out.println("Площидь квадрата со стороной " + s.l + " = " + s.area());
 
     Rectangle r = new Rectangle(4,6);
-    System.out.println("Площидь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
+    System.out.println("Площидь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
   }
 
 
@@ -19,11 +19,5 @@ public class MyFirstProgram {
     System.out.println("Hello, " + somebody + "!");
   }
 
-  public static double area(Square s) {
-    return s.l * s.l;
-  }
 
-  public static double area(Rectangle r) {
-    return r.a * r.b;
-  }
 }
