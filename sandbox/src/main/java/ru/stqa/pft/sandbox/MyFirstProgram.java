e8d75df8e95b3b5ccc3209de88c658d3144ea68e
vi26 <44754162+vi26@users.noreply.github.com>
2018-11-05 18:12:23 +0300

    Добавлены классы и функции

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,11 +7,11 @@ public class MyFirstProgram {
     hello("user");
     hello("Vitaly");
 
-    double l = 5;
-    System.out.println("Площидь квадрата со стороной " + l + " = " + area(l));
-    double a = 4;
-    double b = 6;
-    System.out.println("Площидь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+    Square s = new Square(5);
+    System.out.println("Площидь квадрата со стороной " + s.l + " = " + area(s));
+
+    Rectangle r = new Rectangle(4,6);
+    System.out.println("Площидь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
   }
 
 
@@ -19,11 +19,11 @@ public class MyFirstProgram {
     System.out.println("Hello, " + somebody + "!");
   }
 
-  public static double area(double len) {
-    return len * len;
+  public static double area(Square s) {
+    return s.l * s.l;
   }
 
-  public static double area(double a, double b) {
-    return a * b;
+  public static double area(Rectangle r) {
+    return r.a * r.b;
   }
 }
