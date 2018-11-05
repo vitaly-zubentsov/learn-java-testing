369da1f05f79601db4ba5380b9c2fc63e3c0728b
vi26 <44754162+vi26@users.noreply.github.com>
2018-11-05 20:07:05 +0300

    Вычисление растояния происходит теперь с помощью метода в классе Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
@@ -7,10 +7,8 @@ public class Distance {
     Point p1 = new Point(-1, -1);
     Point p2 = new Point(2, 2);
 
-    System.out.println("Растояние между двумя точками = " + distance(p1, p2));
+    System.out.println("Растояние между двумя точками = " + p1.distance(p2));
   }
 
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt(((p1.x - p2.x) * (p1.x - p2.x)) + ((p1.y - p2.y) * (p1.y - p2.y)));
-  }
+ 
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -8,4 +8,8 @@ public class Point {
     this.x = x;
     this.y = y;
   }
+
+  public double distance(Point p2) {
+    return Math.sqrt(((this.x - p2.x) * (this.x - p2.x)) + ((this.y - p2.y) * (this.y - p2.y)));
+  }
 }
