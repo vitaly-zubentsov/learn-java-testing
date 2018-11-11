7fe5fc7ab974d6be4d6ec05de5a098214334d28c
vi26 <44754162+vi26@users.noreply.github.com>
2018-11-11 13:57:57 +0300

    sandbox for

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PrimesTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PrimesTests {
+
+  @Test
+  public void testPrime() {
+    Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
+  }
+
+  @Test
+  public void testNonPrime() {
+    Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE-2));
+  }
+}
