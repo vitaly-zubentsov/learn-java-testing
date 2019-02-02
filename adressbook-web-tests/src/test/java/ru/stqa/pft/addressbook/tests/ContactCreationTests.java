a386712e3945445b69cc350f15dc83411fbd6cd5
vi26 <44754162+vi26@users.noreply.github.com>
2019-02-02 17:55:07 +0300

    Урок 6, дз. Задание №15, Реализованны провайдеры тестовых данных для тестов контактов

--- adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,20 +1,47 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
+    @DataProvider
+    public Iterator<Object[]> validContactsFromJson() throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")))) {
+            String json = "";
+            String line = reader.readLine();
+            while (line != null) {
+                json += line;
+                line = reader.readLine();
+            }
+            Gson gson = new Gson();
+            List<ContactData> contact = gson.fromJson(json, new TypeToken<List<ContactData>>() {
+            }.getType()); //List <ContactDara>.class
+            return contact.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+        }
+    }
 
-    @Test
-    public void testsContactCreation() {
+
+    @Test(dataProvider = "validContactsFromJson")
+    public void testsContactCreation(ContactData contact) {
+
         app.goTo().homePage();
         Contacts before = app.contact().all();
-        ContactData contact = new ContactData().withFirstName("testFirstName").withLastName("testLastName").withAddress("testAddress").withEmail("testEmail").withEmail2("testEmail2").withEmail3("testEmail3").withPhoneWork("testPhoneWork").withPhoneHome("testPhoneHome").withPhoneMobile("testPhoneMobile");
         app.goTo().addNewPage();
         app.contact().create(contact);
         Contacts after = app.contact().all();
