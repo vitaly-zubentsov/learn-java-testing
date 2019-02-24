811b54bcbe39b1b0b5ea75bb3bd3b9dc3a8bed59
vi26 <44754162+vi26@users.noreply.github.com>
2019-02-24 14:26:25 +0300

    8 урок. Реализован тест проверки регистрации. Использован механизм получения почты, на встроенный в тест почтовый сервер.

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -1,17 +1,24 @@
 package ru.stqa.pft.mantis.appmanager;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
 
-public class RegistrationHelper {
-    private final ApplicationManager app;
-    private WebDriver wd;
+public class RegistrationHelper extends HelperBase {
 
-    public RegistrationHelper(ApplicationManager app) {
-        this.app = app;
-        wd = app.getDriver();
+      public RegistrationHelper(ApplicationManager app) {
+        super(app);
     }
 
     public void start(String username, String email) {
-        wd.get(app.getProperty("web.baseUrl") + "signup_page.php");
+        wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
+        type(By.cssSelector("[name=username]"), username);
+        type(By.cssSelector("[name=email]"), email);
+        click(By.cssSelector("input[value ='Signup']"));
+    }
+
+    public void finish(String confirmationLink, String password) {
+        wd.get(confirmationLink);
+        type(By.cssSelector("[name=password]"), password);
+        type(By.cssSelector("[name= 'password_confirm']"), password);
+        click(By.cssSelector("input[value ='Update User']"));
     }
 }
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistationTests.java
@@ -1,11 +1,47 @@
 package ru.stqa.pft.mantis.tests;
 
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.lanwen.verbalregex.VerbalExpression;
+import ru.stqa.pft.mantis.model.MailMessage;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.List;
+
+import static org.testng.AssertJUnit.assertTrue;
 
 public class RegistationTests extends TestBase {
 
+    @BeforeMethod
+    public void startMailServer() {
+        app.mail().start();
+    }
+
     @Test
-    public void testRegistation() {
-        app.registration().start("user1", "user1@localhost.localhomain");
+    public void testRegistation() throws IOException, MessagingException {
+        long now = System.currentTimeMillis();
+        String user1 = String.format("user%s", now);
+        String password = "password";
+        String email = String.format("user%s@localhost.localhomain", now);
+        app.registration().start(user1, email);
+        List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+        String confirmationLink = findConfirmationLink(mailMessages, email);
+        app.registration().finish(confirmationLink, password);
+        assertTrue(app.newSession().login(user1, password));
+
+    }
+
+    private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
+        MailMessage mailMessage = mailMessages.stream().filter((m) -> m.to.equals(email)).findFirst().get();
+        VerbalExpression regex = VerbalExpression.regex().find("http://").nonSpace().oneOrMore().build();
+        return regex.getText(mailMessage.text);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void stopMailServer() {
+        app.mail().stop();
     }
 }
