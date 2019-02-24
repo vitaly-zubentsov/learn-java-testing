0aade6612ad5b150085c56084f970d8959d9e968
vi26 <44754162+vi26@users.noreply.github.com>
2019-02-24 15:16:59 +0300

    8 урок. Реализован тест проверки регистрации c использован внешней почты james

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistationTestsWithJames.java
@@ -0,0 +1,38 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.testng.annotations.Test;
+import ru.lanwen.verbalregex.VerbalExpression;
+import ru.stqa.pft.mantis.model.MailMessage;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.List;
+
+import static org.testng.AssertJUnit.assertTrue;
+
+public class RegistationTestsWithJames extends TestBase {
+
+
+    @Test
+    public void testRegistation() throws IOException, MessagingException {
+        long now = System.currentTimeMillis();
+        String user = String.format("user%s", now);
+        String password = "password";
+        String email = String.format("user%s@localhost.localhomain", now);
+        app.james().createUser(user, password);
+        app.registration().start(user, email);
+        List<MailMessage> mailMessages = app.james().waitForMail(user, password, 60000);
+        String confirmationLink = findConfirmationLink(mailMessages, email);
+        app.registration().finish(confirmationLink, password);
+        assertTrue(app.newSession().login(user, password));
+
+    }
+
+    private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
+        MailMessage mailMessage = mailMessages.stream().filter((m) -> m.to.equals(email)).findFirst().get();
+        VerbalExpression regex = VerbalExpression.regex().find("http://").nonSpace().oneOrMore().build();
+        return regex.getText(mailMessage.text);
+    }
+
+
+}
