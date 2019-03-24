d35cd4112fc250b7324feb4d87d5efa80531669b
vi26 <44754162+vi26@users.noreply.github.com>
2019-03-24 17:15:43 +0300

    10 урок. Обновил старые тесты для использования с jenkins

--- sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -5,11 +5,12 @@ import org.testng.annotations.Test;
 
 
 public class SquareTests {
-  
-  @Test
-  public void testArea() {
-    Square s = new Square(5);
-    Assert.assertEquals( s.area(),25.0);
-            }
 
-            }
+    @Test
+    public void testArea() {
+        Square s = new Square(5);
+        Assert.assertEquals(s.area(), 25.0);
+        System.out.println("testArea");
+    }
+
+}
